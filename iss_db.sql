e8e8358ac58b15a0ee3bca8716bb165d4d9dddf4
LWhite200 <157498802+LWhite200@users.noreply.github.com>
2025-03-19 11:40:01 -0400

    Add files via upload

---
+++ iss_db.sql
@@ -0,0 +1,136 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 19, 2025 at 04:39 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `iss_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `iss_comments`
+--
+
+CREATE TABLE `iss_comments` (
+  `id` int(11) NOT NULL,
+  `per_id` int(11) NOT NULL,
+  `iss_id` int(11) NOT NULL,
+  `short_comment` varchar(255) NOT NULL,
+  `long_comment` text NOT NULL,
+  `posted_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `iss_issues`
+--
+
+CREATE TABLE `iss_issues` (
+  `id` int(11) NOT NULL,
+  `short_description` varchar(255) NOT NULL,
+  `long_description` text NOT NULL,
+  `open_date` date NOT NULL,
+  `close_date` date NOT NULL,
+  `priority` varchar(255) NOT NULL,
+  `org` varchar(255) NOT NULL,
+  `project` varchar(255) NOT NULL,
+  `per_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `iss_issues`
+--
+
+INSERT INTO `iss_issues` (`id`, `short_description`, `long_description`, `open_date`, `close_date`, `priority`, `org`, `project`, `per_id`) VALUES
+(3, 'short', 'long', '0000-00-00', '0000-00-00', 'xdrg', 'dzfg', 'zdfg', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `iss_persons`
+--
+
+CREATE TABLE `iss_persons` (
+  `id` int(11) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `mobile` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `pwd_hash` varchar(255) NOT NULL,
+  `pwd_salt` varchar(255) NOT NULL,
+  `admin` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `iss_persons`
+--
+
+INSERT INTO `iss_persons` (`id`, `fname`, `lname`, `mobile`, `email`, `pwd_hash`, `pwd_salt`, `admin`) VALUES
+(1, 'George', 'Corser', '', '', '', '', ''),
+(2, 'Lukas', 'White', '911', 'l@w', '$2y$10$ue0FjArYOvnwO/tXjs2EO.L7cvGsnuAWRB3KjZJvMRyTyNg65MPpG', '$2y$10$ue0FjArYOvnwO/tXjs2EO.', '1');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `iss_comments`
+--
+ALTER TABLE `iss_comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `iss_issues`
+--
+ALTER TABLE `iss_issues`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `iss_persons`
+--
+ALTER TABLE `iss_persons`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `iss_comments`
+--
+ALTER TABLE `iss_comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `iss_issues`
+--
+ALTER TABLE `iss_issues`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `iss_persons`
+--
+ALTER TABLE `iss_persons`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
